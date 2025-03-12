f453ec898dcc88c6097ae17ded52be17019f60cc
emirisuu <emilis.mike@gmail.com>
2025-03-13 01:27:32 +0200

    .

--- Crystal/crystal/luhn/src/luhn.cr
+++ Crystal/crystal/luhn/src/luhn.cr
@@ -1,5 +1,17 @@
 module Luhn
   def self.valid?(input : String) : Bool
-    # Write your code for the 'Luhn' exercise in this file.
+    input = input.chars.reject &.whitespace?
+    return false if input.size <= 1
+    sum = 0
+    input.reverse.each_with_index do |number, i|
+      temp = number.to_i?
+      return false if temp.nil?
+      temp = temp * (1 + i % 2)
+      temp -= 9 if temp > 9
+      sum += temp
+    end
+    (sum % 10).zero?
   end
 end
+
+puts Luhn.valid?(" 0")
