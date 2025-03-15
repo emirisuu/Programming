94cdef50b23572cac8d921981f80da19b3553836
emirisuu <emilis.mike@gmail.com>
2025-03-15 11:40:14 +0200

    .

--- Crystal/crystal/queen-attack/src/queen_attack.cr
+++ Crystal/crystal/queen-attack/src/queen_attack.cr
@@ -1,5 +1,5 @@
 class Queen
-  def initialize(x : Int32, y : Int32) : Bool
+  def initialize(x : Int32, y : Int32)
     @chessboard = 
     [
       [0,0,0,0,0,0,0,0],
@@ -11,8 +11,9 @@ class Queen
       [0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0]
     ]
-    raise ArgumentError if x < 0 || x > 7 || y < 0 || y > 7
-
+    if x < 0 || x > 7 || y < 0 || y > 7
+      raise ArgumentError.new("error")
+    end
     @x = x
     @y = y
     @chessboard[x][y] = 1
@@ -26,9 +27,53 @@ class Queen
     @y
   end
 
-  def self.can_attack?(Queen1)
-    @chessboard[Queen1.x][Queen1.y] = 1
-    return true if @chessboard[@x].count(1) > 1
+  def can_attack?(input : Queen) : Bool
+    @chessboard[input.x][input.y] = 1
     
+    return true if @chessboard[@x].count(1) > 1
+
+    i = 0
+    counter = 0
+    while i < 8
+      counter += 1 if @chessboard[i][@y] == 1
+      return true if counter > 1
+      i += 1
+    end
+    
+
+    temp_x = @x
+    temp_y = @y
+    while temp_x > 0 && temp_y > 0
+      temp_x -= 1
+      temp_y -= 1
+    end
+
+    counter = 0
+    while temp_x < 8 && temp_y < 8
+      counter += 1 if @chessboard[temp_x][temp_y] == 1
+      return true if counter > 1
+      temp_x += 1
+      temp_y += 1
+    end
+    
+
+    temp_x = @x
+    temp_y = @y
+    while temp_x > 0 && temp_y > 0
+      temp_x += 1
+      temp_y -= 1
+    end
+
+    counter = 0
+    while temp_x >= 0 && temp_y < 8
+      counter += 1 if @chessboard[temp_x][temp_y] == 1
+      return true if counter > 1
+      temp_x -= 1
+      temp_y += 1
+    end
+    
+    false
   end
 end
+
+#puts Queen.new(2, 2).can_attack?(Queen.new(0, 4))
