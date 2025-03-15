05939e0213cc015a840ad2ed1dbfc6c1c9cbdac3
emirisuu <emilis.mike@gmail.com>
2025-03-15 11:40:00 +0200

    .

--- Crystal/crystal/queen-attack/src/queen_attack.cr
+++ Crystal/crystal/queen-attack/src/queen_attack.cr
@@ -1,3 +1,34 @@
 class Queen
-  # Write your code for the 'Queen Attack' exercise in this file.
+  def initialize(x : Int32, y : Int32) : Bool
+    @chessboard = 
+    [
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0]
+    ]
+    raise ArgumentError if x < 0 || x > 7 || y < 0 || y > 7
+
+    @x = x
+    @y = y
+    @chessboard[x][y] = 1
+  end
+
+  def x
+    @x
+  end
+
+  def y
+    @y
+  end
+
+  def self.can_attack?(Queen1)
+    @chessboard[Queen1.x][Queen1.y] = 1
+    return true if @chessboard[@x].count(1) > 1
+    
+  end
 end
