20036dc0d381cbd77f881cdf6428adf43f7460a1
emirisuu <emilis.mike@gmail.com>
2025-03-25 15:31:04 +0200

    loxas esu

--- Crystal/crystal/anagram/src/anagram.cr
+++ Crystal/crystal/anagram/src/anagram.cr
@@ -1,23 +1,24 @@
 module Anagram
   def self.find(subject : String, candidates : Array(String)) : Array(String)
     subject.downcase
-    candidates.each_with_index do |word, index|
-      unless word.size == subject.size
-        candidates.delete index
-      end
-    end
     dup_candidates = candidates.dup.map &.downcase
-    subject_counter = 0
+    subject_array = Array(Char).new
     subject.each_char do |char|
-      subject_counter += char.to_i
+      subject_array << char
     end
     dup_candidates.each_with_index do |word, index|
-      word_counter = 0
-      word.each_char do |char|
-        word_counter += char.to_i
-      end
-      unless word_counter == subject_counter
-        candidates.delete index
+      if word.downcase == subject || word.size != subject.size
+        candidates.delete_at index
+        dup_candidates.delete_at index
+      else
+        word_array = Array(Char).new
+        word.each_char do |char|
+          word_array << char
+        end
+        unless word_array.sort == subject_array.sort
+          candidates.delete_at index
+          dup_candidates.delete_at index
+        end
       end
     end
     candidates
