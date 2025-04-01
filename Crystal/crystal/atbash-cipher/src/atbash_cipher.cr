d00aac62292b5b63581225f52221300427370e51
emirisuu <emilis.mike@gmail.com>
2025-04-01 15:03:56 +0300

    unfinished crystal

--- Crystal/crystal/atbash-cipher/src/atbash_cipher.cr
+++ Crystal/crystal/atbash-cipher/src/atbash_cipher.cr
@@ -1,6 +1,11 @@
 module AtbashCipher
   # Write your code for the 'Atbash Cipher' exercise in this file.
   def self.encode(message : String) : String
+    message = message.downcased 
+    message = message.gsub(" ", "")
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    cipher = alphabet.reversed
+    
   end
 
   def self.decode(message : String) : String
