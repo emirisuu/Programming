8499e4815523051bed7bc34fa92d18b2ddf62f45
emirisuu <emilis.mike@gmail.com>
2025-04-08 02:03:13 +0300

    unfinished crystal again

--- Crystal/crystal/atbash-cipher/src/atbash_cipher.cr
+++ Crystal/crystal/atbash-cipher/src/atbash_cipher.cr
@@ -1,11 +1,24 @@
 module AtbashCipher
   # Write your code for the 'Atbash Cipher' exercise in this file.
   def self.encode(message : String) : String
-    message = message.downcased 
+    message = message.downcase
     message = message.gsub(" ", "")
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    cipher = alphabet.reversed
-    
+    cipher = alphabet.reverse
+    encoded_message = ""
+    temp_message = ""
+    message.each_char_with_index do |char, index|
+      if char.letter?
+        temp_message += cipher[alphabet.index char]
+      elsif char.number?
+        temp_message += char
+      end
+      if temp_message.size == 5
+        encoded_message += temp_message + " "
+        temp_message = ""
+      end
+    end
+    encoded_message
   end
 
   def self.decode(message : String) : String
