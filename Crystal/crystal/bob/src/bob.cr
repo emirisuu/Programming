a9c3b8c3a4a202b754d267fafdc583d916f6f80b
emirisuu <emilis.mike@gmail.com>
2025-04-28 21:05:46 +0300

    lazy to finish crystal

--- Crystal/crystal/bob/src/bob.cr
+++ Crystal/crystal/bob/src/bob.cr
@@ -1,11 +1,11 @@
 module Bob
   def self.hey(string : String) : String
     string = string.strip.gsub("\t", "")
+    temp = string.upcase
     case string
     when string.empty? || string.blank? then "Fine. Be that way!"
-    when string[-1] == '?' then string == string.upcase ? "Calm down, I know what I'm doing!" : "Sure."
-    when string.empty? || string.blank? then "Fine. Be that way!"
-    else string == string.upcase ? "Whoa, chill out!" : "Whatever."
+    when string[-1] == '?' then string == temp ? "Calm down, I know what I'm doing!" : "Sure."
+    else string == temp ? "Whoa, chill out!" : "Whatever."
     end
   end
 end
