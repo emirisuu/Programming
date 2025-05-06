570e6df5a074f2080ca289ac8e5be1caa2edf4aa
emirisuu <emilis.mike@gmail.com>
2025-05-06 22:53:59 +0300

    Added benchmarking and testing to edmond and karp algorithm

--- Crystal/Edmonds & Karp/edmonds & karp.cr
+++ Crystal/Edmonds & Karp/edmonds & karp.cr
@@ -1,3 +1,5 @@
+require "benchmark"
+
 class Graph
     def initialize(@size : Int32)
         @adj_matrix = Array(Array(Int32)).new(@size) { Array(Int32).new(@size, 0) }
@@ -65,21 +67,34 @@ class Graph
     end
 end
 
-g = Graph.new(6)
-vertex_names = ["s", "v1", "v2", "v3", "v4", "t"]
-vertex_names.each_with_index do |name, index|
-    g.add_vertex_data index,name
+def test_algorithm(data_amount : Int32)
+    g = Graph.new(data_amount)
+    (0...data_amount).each do |index|
+        g.add_vertex_data index, index.to_s
+    end
+
+    (0...data_amount - 1).each do |index|
+        3.times do
+            g.add_edge index, Random.rand(data_amount), Random.rand 1..10
+        end
+    end
+
+    sink = [5, data_amount - 1].min #Use 5 or the last vertex if data_amount is less than 5
+    total_time = Benchmark.measure {g.edmonds_karp 0, sink}
+    total_time
 end
 
-g.add_edge(0, 1, 3)  # s  -> v1, cap: 3
-g.add_edge(0, 2, 7)  # s  -> v2, cap: 7
-g.add_edge(1, 3, 3)  # v1 -> v3, cap: 3
-g.add_edge(1, 4, 4)  # v1 -> v4, cap: 4
-g.add_edge(2, 1, 5)  # v2 -> v1, cap: 5
-g.add_edge(2, 4, 3)  # v2 -> v4, cap: 3
-g.add_edge(3, 4, 3)  # v3 -> v4, cap: 3
-g.add_edge(3, 5, 2)  # v3 -> t,  cap: 2
-g.add_edge(4, 5, 6)  # v4 -> t,  cap: 6
-source = 0
-sink = 5
-puts "The maximum possible flow is #{g.edmonds_karp(source, sink)}"
+average_times = Array(Float64).new
+
+(1..11).each do |index|
+    run_total_time = 0
+
+    20.times do
+        run_total_time += test_algorithm(index * 1000).total
+    end
+    average_times << run_total_time / 20
+end
+
+average_times.each_with_index do |value, index|
+    puts "Size #{index * 1000}, Time taken #{value}s"
+end
